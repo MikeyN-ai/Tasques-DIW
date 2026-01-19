4d59d1ca2d24d4f87a9211bf6abf22342ad82b3b
MikeyN-ai <migcabgar@alu.edu.gva.es>
2026-01-19 11:25:35 +0100

    boostrap casi acabado

--- sprint4-backoffice/js/common.js
+++ sprint4-backoffice/js/common.js
@@ -5,16 +5,12 @@ function main() {
     // Configuració del botó de hamburguesa
     const menu_hamburguesa = document.getElementById("menu_hamburguesa");
     const nav = document.getElementById("menu_lateral");
-    let visible = false;
 
     // Gestió del menú de hamburguesa
     menu_hamburguesa.addEventListener("click", (e) => {
-        if (visible) {
-            nav.style.display = 'none';
-            visible = false;
-        } else {
-            nav.style.display = 'block';
-            visible = true;
-        }
+        nav.classList.toggle("menu_visible");
+        nav.classList.toggle("menu_lateral_fixed");
     });
 }
+
+const bsButton = new bootstrap.Button('#myButton');
