07e0aaee35a38ce0df8b5a693a3206ed078de1a7
MikeyN-ai <migcabgar@alu.edu.gva.es>
2026-05-05 10:24:58 +0200

    avances grandes de graficos y elementos generales

---
+++ sprint5-backoffice/js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", main)
+
+function main() {
+    new Chart(document.getElementById("pie-chart"), {
+        type: 'pie',
+        data: {
+            labels: ["Africa", "Asia", "Amèrica"],
+            datasets: [{
+                label: "Population (millions)",
+                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
+                data: [150, 300, 50]
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Predicted world population (millions) in 2050'
+            }
+        }
+    });
+
+}
